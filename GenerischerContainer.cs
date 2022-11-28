e5d81f4227e892aa46dd15a3920834564b55116a
k s <klaus.stolz@web.de>
2022-11-28 11:29:54 +0100

    Add project files.

---
+++ GenerischerContainer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsBeiispiel {
+    class GenerischerContainer<T> {
+        // Liste eines generischen Typs
+        private List<T> meineListe = new List<T>();
+
+        public T Element { get; set; }
+
+        public void Einfuegen ( T element ) {
+            meineListe.Add ( element );
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsBeiispiel {
+    class Program {
+        static void Main ( string [ ] args ) {
+            GenerischerContainer<string> gc = new GenerischerContainer<string> ( );
+            gc.Element = "robert";
+            gc.Einfuegen ( "4711-String" );
+
+            GenerischerContainer<int> gcint = new GenerischerContainer<int> ( );
+            gcint.Element = 67;
+            gcint.Einfuegen ( 4711 );
+        }
+    }
+}
